81360e024d37f8a62175e110ce6b1a121f3ffa5c
Ross Jourdain <ross.jourdain@gmail.com>
2019-02-19 10:02:28 +1300

    Dummy API

---
+++ server/routes/tweets.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+// GET /api/v1/tweets
+router.get('/', (req,res) => {
+  res.json([
+    {
+      id: 5,
+      text: 'This is a tweet'
+    }
+  ])
+})
+
+// POST /api/v1/tweets
+router.post('/', (req,res) => {
+  res.json({ id: 6 })
+})
+
+// GET /api/v1/tweets/:username
+router.get('/:username', (req,res) => {
+  res.json([
+    {
+      id: 5,
+      text: 'This is a tweet'
+    }
+  ])
+})
+
+module.exports = router
